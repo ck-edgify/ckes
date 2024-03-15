3ae778c8e9267a2c8a40231350e726f6263b0e62
ck-edgify <163007302+ck-edgify@users.noreply.github.com>
2024-03-15 10:09:58 +0000

    Create get_imgs.sh

---
+++ get_imgs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Create directory if it doesn't exist
+mkdir -p /edgify_agent/images
+
+# Change directory to /edgify_agent/images
+cd /edgify_agent/images || exit
+
+# Array of image URLs
+images=(
+    "https://github.com/ck-edgify/ckes/raw/main/apple-rd.jpg"
+    "https://github.com/ck-edgify/ckes/raw/main/lucozade.jpg"
+    "https://github.com/ck-edgify/ckes/raw/main/penguin.png"
+    "https://github.com/ck-edgify/ckes/raw/main/plushie.png"
+    "https://github.com/ck-edgify/ckes/raw/main/puzzle.png"
+    "https://github.com/ck-edgify/ckes/raw/main/scrumbles-chicken.jpg"
+    "https://github.com/ck-edgify/ckes/raw/main/scrumbles-salmon.jpg"
+)
+
+# Loop through each image URL and download it
+for url in "${images[@]}"; do
+    wget -q --show-progress "$url"
+done
+
+echo "All files downloaded successfully."
+
+# Create an empty config.json file
+cd /edgify_agent || exit
+touch config.json
+
+echo "config.json created successfully."
